e850806690cdb1a354d28f09b4b268de5f8b598b
Tanish13 <75306892+Tanish13@users.noreply.github.com>
2021-01-31 22:24:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+var dustbinObj,groundObject	
+var world,paperImg,paper1;
+
+function preload(){
+	paperImg = loadImage("paper.png");
+}
+function setup() {
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+
+
+	engine = Engine.create();
+	world = engine.world;
+	
+	groundObject=new ground(width/2,670,width,20);
+	dustbinObj=new dustbin(1200,650);
+	paper1 = new paper (200,630);
+	
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(230);
+ 
+
+  groundObject.display();
+  dustbinObj.display();
+  paper1.display();
+
+}
+
